e14f85c2ba91265cda2f933e1debcc89553273fe
Quilfort Frederiks <qfrederi@f1r2s13.codam.nl>
2022-06-09 16:39:51 +0200

    Dit is voor je moeder

--- Src/main.c
+++ Src/main.c
@@ -6,7 +6,7 @@
 /*   By: qfrederi <qfrederi@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/06/02 15:18:45 by qfrederi      #+#    #+#                 */
-/*   Updated: 2022/06/09 16:28:54 by qfrederi      ########   odam.nl         */
+/*   Updated: 2022/06/09 16:39:04 by qfrederi      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,4 +72,4 @@ int	main(int argc, char *argv[])
 	return (0);
 }
 
-// QUILFORT'S TEST TO BRANCHE
+// Dit is de Branche van Rolf
